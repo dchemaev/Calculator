4f37655312182ed64e284c2eba9cb0169f010d29
Danil <45203038+dchemaev@users.noreply.github.com>
2020-08-05 21:16:09 +0400

    In progress

--- Main.py
+++ Main.py
@@ -3,17 +3,17 @@ from PyQt5.QtCore import Qt
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 from Calculation import *
+from NumSysCalc import NumSysCalculation
 
-from Calculator.Calculation import ReversePolishNotationClass, ReaderClass
+from Calculator.Calculation import ReversePolishNotationClass
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.secondWin = None
+    def __init__(self):
+        super().__init__()
 
         self.initUI()
         self.setWindowIcon(QIcon("Images/calc_Icon.png"))
@@ -59,16 +59,14 @@ class MainWindow(QMainWindow):
 
         self.result_show.setReadOnly(True)  # Нельзя ничего добавить в дисплей с клавиатуры
 
-        self.pushButton_del.clicked.connect(lambda: self.clear(True))  # кнопка AC
-        self.pushButton_del_2.clicked.connect(lambda: self.clear(False))  # кнопка del
+        self.pushButton_AC.clicked.connect(lambda: self.clear(True))  # кнопка AC
+        self.pushButton_del.clicked.connect(lambda: self.clear(False))  # кнопка del
 
-        self.pushButton_NumSysCalc.clicked.connect(self.openWindow)
+        self.pushButton_NumSysCalc.clicked.connect(self.openDialog)
 
-    def openWindow(self):
-        if not self.secondWin:
-            self.secondWin = NumSystemWindow(self)
-        self.secondWin.setWindowIcon(QIcon("Images/calc_Icon.png"))
-        self.secondWin.show()
+    def openDialog(self):
+        dialog = NumSystemWindow(self)
+        dialog.show()
 
     def initUI(self):
         uic.loadUi("UI/calculatorDesign.ui", self)
@@ -77,8 +75,7 @@ class MainWindow(QMainWindow):
         # self.result_show.insert(value)
         self.result_show.setText(self.result_show.toPlainText() + value)  # Последовательный ввод символов
 
-    def keyPressEvent(self, event):
-        # интеграция окна калькулятора с клавиатурой и NumPad
+    def keyPressEvent(self, event):  # интеграция окна калькулятора с клавиатурой и NumPad
         if event.key() == Qt.Key_1:
             self.display('1')
         if event.key() == Qt.Key_2:
@@ -153,11 +150,98 @@ class MainWindow(QMainWindow):
 
 class NumSystemWindow(QMainWindow):
     def __init__(self, parent=None):
-        super().__init__(parent, QtCore.Qt.Window)
+        super(NumSystemWindow, self).__init__(parent)
+
         self.initUI()
+        self.setWindowIcon(QIcon("Images/calc_Icon.png"))
+
+        self.pushButton_0.clicked.connect(lambda: self.display("0"))
+        self.pushButton_1.clicked.connect(lambda: self.display("1"))
+        self.pushButton_2.clicked.connect(lambda: self.display("2"))
+        self.pushButton_3.clicked.connect(lambda: self.display("3"))
+        self.pushButton_4.clicked.connect(lambda: self.display("4"))
+        self.pushButton_5.clicked.connect(lambda: self.display("5"))
+        self.pushButton_6.clicked.connect(lambda: self.display("6"))
+        self.pushButton_7.clicked.connect(lambda: self.display("7"))
+        self.pushButton_8.clicked.connect(lambda: self.display("8"))
+        self.pushButton_9.clicked.connect(lambda: self.display("9"))
+
+        self.pushButton_OK.clicked.connect(self.do)
+        self.import_display.setReadOnly(True)
+        self.export_display.setReadOnly(True)
+
+        self.pushButton_AC.clicked.connect(lambda: self.clear(True))  # кнопка AC
+        self.pushButton_del.clicked.connect(lambda: self.clear(False))  # кнопка del
+
+    def check(self):
+        if self.import_rbt_2.isChecked():
+            i = 2
+        if self.import_rbt_3.isChecked():
+            i = 3
+        if self.import_rbt_4.isChecked():
+            i = 4
+        if self.import_rbt_5.isChecked():
+            i = 5
+        if self.import_rbt_6.isChecked():
+            i = 6
+        if self.import_rbt_7.isChecked():
+            i = 7
+        if self.import_rbt_8.isChecked():
+            i = 8
+        if self.import_rbt_9.isChecked():
+            i = 9
+        if self.import_rbt_10.isChecked():
+            i = 10
+
+        if self.export_rbt_2.isChecked():
+            j = 2
+        if self.export_rbt_3.isChecked():
+            j = 3
+        if self.export_rbt_4.isChecked():
+            j = 4
+        if self.export_rbt_5.isChecked():
+            j = 5
+        if self.export_rbt_6.isChecked():
+            j = 6
+        if self.export_rbt_7.isChecked():
+            j = 7
+        if self.export_rbt_8.isChecked():
+            j = 8
+        if self.export_rbt_9.isChecked():
+            j = 9
+        if self.export_rbt_10.isChecked():
+            j = 10
+
+        print(i, j)
+        return i, j
+
+    def do(self):
+        text = NumSysCalculation(self.import_display.toPlainText().split())
+        rez = text.calculation(*self.check())
+
+        self.export_display.setText(str(int(*rez)))
 
     def initUI(self):
         uic.loadUi("UI/NumSystem.ui", self)
+
+    def display(self, value):  # Показывает вводимые символы на "экранчик"
+        # self.result_show.insert(value)
+        self.import_display.setText(self.import_display.toPlainText() + value)  # Последовательный ввод символов
+
+    def clear(self, rez):  # очистка дисплейчика
+        if rez:  # функция AC
+            self.import_display.setText("")
+        elif not rez:  # функция del
+            txt = []
+            for i in self.import_display.toPlainText():
+                txt.append(i)
+            txt = txt[:-1]  # удаляем послений элемент
+            if len(txt) > 0:  # меняем значение на экранчике
+                self.import_display.setText("")
+                for j in txt:
+                    self.import_display.setText(self.import_display.toPlainText() + j)
+            else:
+                self.clear(True)
 
 
 if __name__ == "__main__":  # Обработка клацаний юзера
